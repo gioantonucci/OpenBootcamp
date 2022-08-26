14b4f395f4f73986d10f6d3efc0088ec4916d702
gioantonucci <gioantonucci@hotmail.com>
2022-08-26 17:57:47 -0300

    estructuras

---
+++ JavaScript/Estructuras de control/factorial-for.js
@@ -0,0 +1,8 @@
+// - factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+const value = 10;
+let aux = 1;
+
+for (let i = 1; i < value; i++) {
+    let valor = aux*=i 
+    console.log(valor)
+}
---
+++ JavaScript/Estructuras de control/factorial-while.js
@@ -0,0 +1,11 @@
+// - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let value = 10;
+let aux = 1;
+
+while (value != 0 ) {
+    aux = aux * value;
+    value = value -1;
+    }
+
+    console.log(aux)
