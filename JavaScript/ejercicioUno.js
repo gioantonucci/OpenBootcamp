82b45c04ad44955b3c94e4618df5075db96c9eaf
gioantonucci <gioantonucci@hotmail.com>
2022-08-26 14:13:52 -0300

    uno

---
+++ JavaScript/ejercicioUno.js
@@ -0,0 +1,22 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+const name = 'Giovanna';
+console.log(name)
+// - Tu edad (number)
+const age = 24;
+console.log(age);
+// - ¿Eres desarrollador? (boolean)
+const isDev = true;
+console.log(isDev);
+// - Tu fecha de nacimiento (Date)
+const birthday = new Date(1997,8,9)
+console.log(birthday); 
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+const book = {
+    title: 'Secret Ceremony',
+    author: 'Marco Denevi',
+    year: 1961,
+    url: 'https://books.google.com.ar/books/about/Ceremonia_secreta_Secret_Ceremony.html?id=m_i3GQAACAAJ&redir_esc=y'
+
+}
