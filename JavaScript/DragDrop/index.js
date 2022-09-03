63bd89898c7e60872bac40edd71e2a3872576389
gioantonucci <gioantonucci@hotmail.com>
2022-09-03 13:03:04 -0300

    draganddrop

---
+++ JavaScript/DragDrop/index.js
@@ -0,0 +1,37 @@
+// - Replica el ejercicio completo del vídeo
+// - Añade dos párrafos más en cada una de las secciones
+// - Pon un icono de papelera para eliminar párrafos (Esta imagen puede valer: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSMWeEC0tULzOjfWFAOdOHjUw9WjXhBDbirg&usqp=CAU)
+// - Añade el código necesario para que al arrastrar un fragmento del puzzle a la papelera, éste se elimine
+
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".container")
+const tacho = document.querySelectorAll(".drop")
+
+parrafos.forEach(p => {
+    p.addEventListener('dragstart', e => {
+        //console.log('arrastre: ' + p.innerHTML)
+        p.classList.add('dragging')
+        e.dataTransfer.setData('id', p.id)
+        //console.log(p.id)
+        
+    })
+    p.addEventListener('dragend', ()=>{
+        //console.log('deje de arrastrar')
+        p.classList.remove('dragging')
+    })
+})
+
+secciones.forEach(s => {
+    s.addEventListener('dragover', e => {
+        e.preventDefault()
+        //console.log('dragover')
+    })
+    s.addEventListener('drop', e => {
+        //console.log('drop')
+        const id_parrafo = e.dataTransfer.getData('id')
+        //console.log('id:', id_parrafo)
+        const parrafo = document.getElementById(id_parrafo)
+        s.appendChild(parrafo)
+        
+    })
+})
--- JavaScript/Persistencia/index.js
+++ JavaScript/Persistencia/index.js
@@ -3,7 +3,6 @@
 // - Crea un fichero index.js
 // - Integra el fichero index.js en el html
 // - En el fichero index.js:
-
 // - Crea una variable con tu nombre
 let nombre = 'Giovana'
 // - Crea una variable con tu apellido
@@ -13,14 +12,13 @@ let persona = {
     nombre, 
     apellido
 }
-
 // - Almacena el objeto anterior en la SessionStorage
-sessionStorage.setItem("datos", JSON.stringify(persona))
+//sessionStorage.setItem("datos", JSON.stringify(persona))
 // - Almacena el objeto anterior en la LocalStorage
-localStorage.setItem("datos", JSON.stringify(persona))
+//localStorage.setItem("datos", JSON.stringify(persona))
 // - Crea una cookie que caduque en 2 minutos con los datos del objeto anterior
 const now = new Date()
-document.cookie = `datos=${JSON.stringify(persona)};expires=${new Date(now.getTime() + 2 * 60000)}`
+//document.cookie = `datos=${JSON.stringify(persona)};expires=${new Date(now.getTime() + 2 * 60000)}`
 
 // - Observa en Google Chrome cómo se almacenan los datos en la SessionStorage, LocalStorage y las cookies
 // - Cierra el navegador, comenta las líneas que almacenan SessionStorage, LocalStorage y CookieStorage y vuelve a abrirlo
